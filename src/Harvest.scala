d9fdc38bb0995daeb697056fd7fb90bab697afbb
Dhruv Gharat <dhruvrg2003@gmail.com>
2025-04-29 16:14:46 +0530

    Completed Harvest assignment

--- src/Harvest.scala
+++ src/Harvest.scala
@@ -15,8 +15,7 @@ object Harvest extends App {
   }
   pricesFile.close()
 
-  val map: mutable.Map[String, Float] = mutable.Map().withDefaultValue(0.0f)
-  case class Entry(gather: String, date: String, fruit: String, earning: Float)
+  case class Entry(gather: String, date: String, fruit: String, amount: Float, earning: Float)
   val entries = ListBuffer[Entry]()
 
   for (line <- harvestFile.getLines) {
@@ -25,11 +24,69 @@ object Harvest extends App {
     val fruit = line.split(",")(2)
     val amount = line.split(",")(3).toFloatOption.getOrElse(0.0f)
     val price = pricesMap(fruit, date)
-    entries += Entry(name, date, fruit, amount * price)
-    map(name) += amount
+    entries += Entry(name, date, fruit, amount, amount * price)
   }
   harvestFile.close()
 
-  println(s"Best gatherer is ${map.maxBy(pair => pair._2)._1}")
+  val list = entries.tail
 
+  def sum(list: ListBuffer[Entry]): Float = {
+    if (list.isEmpty) 0
+    else list.head.earning + sum(list.tail)
+  }
+
+  def sumAmount(list: ListBuffer[Entry]): Float = {
+    if (list.isEmpty) 0
+    else list.head.amount + sumAmount(list.tail)
+  }
+
+  def bestFruitOfMonth(groupData: ListBuffer[Entry]): String = {
+    val data = groupData.groupBy(pair => pair.fruit)
+    data.maxBy(fruit => sum(fruit._2))._1
+  }
+
+  def leastFruitOfMonth(groupData: ListBuffer[Entry]): String = {
+    val data = groupData.groupBy(pair => pair.fruit)
+    data.minBy(fruit => sum(fruit._2))._1
+  }
+
+  def mostContributedIncome(groupData: ListBuffer[Entry]): String = {
+    val data = groupData.groupBy(pair => pair.gather)
+    data.maxBy(gather => sum(gather._2))._1
+  }
+
+  def bestGatherOfMonth(groupData: ListBuffer[Entry]): String = {
+    val data = groupData.groupBy(pair => pair.gather)
+    data.maxBy(gather => sumAmount(gather._2))._1
+  }
+
+  def bestGatheringFruit(groupData: ListBuffer[Entry]): String = {
+    val data = groupData.groupBy(pair => pair.fruit)
+    data.maxBy(fruit => sumAmount(fruit._2))._1
+  }
+
+  val groupData = list.groupBy(entry => entry.date.split("-")(1))
+
+  groupData.map(pair => println(s"The best fruit in month ${pair._1} is ${bestFruitOfMonth(pair._2)}"))
+  println("\n")
+  groupData.map(pair => println(s"The least fruit in month ${pair._1} is ${leastFruitOfMonth(pair._2)}"))
+
+  println("\n")
+  val fruitData = list.groupBy(entry => entry.fruit)
+  println(s"The best fruit is ${fruitData.maxBy(entry => sum(entry._2))._1}")
+  println(s"The least fruit is ${fruitData.minBy(entry => sum(entry._2))._1}")
+
+  println("\n")
+  groupData.map(pair => println(s"Most Contributed Income in month ${pair._1} is done by ${mostContributedIncome(pair._2)}"))
+
+  println("\n")
+  val gatherList = list.groupBy(entry => entry.gather)
+  println(s"Most Contributed Income is done by ${gatherList.maxBy(entry => sum(entry._2))._1}")
+
+  println("\n")
+  groupData.map(pair => println(s"The best gather in month ${pair._1} is ${bestGatherOfMonth(pair._2)}"))
+
+  println("\n")
+  gatherList.map(pair => println(s"${pair._1} is good at gathering ${bestGatheringFruit(pair._2)}"))
 }
+
